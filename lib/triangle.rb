cdc148a35d25a02027279883a7d8524fb0f75acf
KellyMark <kmark602@gmail.com>
2023-04-30 21:09:05 -0400

    commit tree 065332a652e8dd4c5c67ed2f45724980ce1e1422

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Triangle
   # write code here
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    if @side_one <= 0 || @side_two <= 0 || @side_three <= 0
+      raise TriangleError
+    elsif @side_one + @side_two <= @side_three || @side_two + @side_three <= @side_one || @side_one + @side_three <= @side_two
+      raise TriangleError
+    elsif @side_one > 0 && @side_two > 0 && @side_three > 0
+      if @side_one == @side_two && @side_two == @side_three && @side_one > 0 && @side_two > 0 && @side_three > 0 
+      return :equilateral
+      elsif @side_two == @side_three || @side_one == @side_three || @side_one == @side_two
+      return :isosceles
+      elsif @side_one != @side_two && @side_two != @side_three && @side_one != @side_three 
+         return :scalene
+      end
+    else
+      raise TriangleError
+    end
+    end
+
+  class TriangleError < StandardError
+
+  end
 end
+
+negative = Triangle.new(-3, -3, -3)
+binding.pry
